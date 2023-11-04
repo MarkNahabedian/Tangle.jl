db3f945b7b5febfc97ef6e748e289d8b67c5987b
MarkNahabedian <naha@mit.edu>
2023-11-04 12:47:44 -0400

    oops.  Missing file.

---
+++ src/symbolics_vector_variables.jl
@@ -0,0 +1,28 @@
+using Symbolics
+using Symbolics: variable
+
+
+"""
+    symbolic_vector(name::Symbol, n::Integer)
+
+Define `n` Symbolics variables hose names are based on `name` and
+return a vector of those variables.
+"""
+function symbolic_vector(name::Symbol, n::Integer)
+    eltname(name, i) = Symbol("($name)_$i")
+    map(variable, map(i -> eltname(name, i), 1:n))
+end
+
+
+"""
+    vsubs(v::Vector{Symbolics.Num}, values...)
+
+Return a list of `Pair`s suitable for constructing a `Dict` which is
+suitable for passing to `Symbolics.substitute`, which
+substitues each of `values` with the corresponding variable in `v`.
+"""
+function vsubs(v::Vector{Symbolics.Num}, values...)
+	Pair.(v, values)
+end
+
+
