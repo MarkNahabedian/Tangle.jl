bcc10dbbc441e193152544c8f63bc78c57f73f9e
MarkNahabedian <naha@mit.edu>
2026-01-27 09:27:10 -0500

    Checkpointb code.  Now using Symbolics.Arr.  Can't find crossings yet but can make the knot function and project it.

--- src/symbolic_cubic_splines.jl
+++ src/symbolic_cubic_splines.jl
@@ -3,6 +3,9 @@ using SymbolicUtils
 using OrderedCollections
 using LinearAlgebra
 
+# Pkg.add("Groebner")
+using Groebner              # for symbolics_solve
+
 using Tangle
 
 #=
@@ -21,10 +24,18 @@ evaluation if necessary.
 
 =#
 
+SYMBOLIC_CUBIC_SPLINES_FOLD = false
+
 function symbolic_cubic_spline(points, looped::Bool)
-    coefficients = []
+    coefficients = Num[]
     equations = []
     polynomials = []
+    massage_kp(kp) =
+        if kp == typemax(KnotParameter)
+            1//1
+        else
+            kp.p
+        end
     @variables kp       # The KnotParameter
     # For N points we have one polynomial for each of the N-1
     # segments.  The end points of those segments are solutions to
@@ -40,25 +51,29 @@ function symbolic_cubic_spline(points, looped::Bool)
         # points[i] is a solutiion to polynomial:
         push!(equations,
               points[i][2] ~ substitute(polynomial,
-                                        Dict(kp => points[i][1].p)))
+                                        Dict(kp => massage_kp(points[i][1]));
+                                        fold = SYMBOLIC_CUBIC_SPLINES_FOLD))
         # So is points[i+1]
         push!(equations,
               points[i+1][2] ~ substitute(polynomial,
-                                          Dict(kp => points[i+1][1].p)))
+                                          Dict(kp => massage_kp(points[i+1][1]));
+                                          fold = SYMBOLIC_CUBIC_SPLINES_FOLD))
     end
     @assert length(polynomials) == length(points) - 1
     ddkp(poly) = expand_derivatives(Differential(kp)(poly))
     function continuous_derivatives(p1, p2, point)
-        subst = Dict(kp => point[1].p)
+        subst = Dict(kp => massage_kp(point[1]))
         # At any given interior point, the first derivatives of the two
         # polynomials adjacent to that point are equal at that point:
         push!(equations,
-              substitute(ddkp(p1), subst) ~ substitute(ddkp(p2), subst))
+              substitute(ddkp(p1), subst) ~ substitute(ddkp(p2), subst;
+                                                       fold = SYMBOLIC_CUBIC_SPLINES_FOLD))
         # At any given interior point, the second derivatives of
         # the two polynomials adjacent to that point are equal at
         # that point:
         push!(equations,
-              substitute(ddkp(ddkp(p1)), subst) ~ substitute(ddkp(ddkp(p2)), subst))
+              substitute(ddkp(ddkp(p1)), subst) ~ substitute(ddkp(ddkp(p2)), subst;
+                                                             fold = SYMBOLIC_CUBIC_SPLINES_FOLD))
     end
     for i in 1 : (length(polynomials) - 1)
         continuous_derivatives(polynomials[i], polynomials[i+1], points[i+1])
@@ -72,9 +87,13 @@ function symbolic_cubic_spline(points, looped::Bool)
         # The second derivatives of the first and last polynomials at the
         # end points are 0:
         push!(equations,
-              0 ~ substitute(ddkp(ddkp(polynomials[1])), Dict(kp => points[1][1].p)))
+              0 ~ substitute(ddkp(ddkp(polynomials[1])),
+                             Dict(kp => massage_kp(points[1][1]));
+                             fold = SYMBOLIC_CUBIC_SPLINES_FOLD))
         push!(equations,
-              0 ~ substitute(ddkp(ddkp(polynomials[end])), Dict(kp => points[end][1].p)))
+              0 ~ substitute(ddkp(ddkp(polynomials[end])),
+                             Dict(kp => massage_kp(points[end][1]));
+                             fold = SYMBOLIC_CUBIC_SPLINES_FOLD))
     end
     # Solve for the coefficients of the polymomials:
     global EQUATIONS = equations
@@ -84,22 +103,21 @@ function symbolic_cubic_spline(points, looped::Bool)
     println("\n*** coefficients:  ", coefficients)      # IT LOOKS LIKE WE'RE NOT SOLVING THE COEFFICIENTS.  They're all Nan.
     # Substitute the coefficients into the polynomials:
     polynomials = map(polynomials) do p
-        substitute(p, coefficients)
+        substitute(p, coefficients; fold = SYMBOLIC_CUBIC_SPLINES_FOLD)
     end
     function make_ifelse(i)
         if i == length(polynomials)
             polynomials[i]
         else
-            Symbolics.ifelse((points[i][1] <= kp) & (kp < points[i + 1][1]),
-                             polynomials[i],
-                             make_ifelse(i + 1))
+            Symbolics.ifelse(
+                (massage_kp(points[i][1]) <= kp) &
+                (kp < massage_kp(points[i + 1][1])),
+                polynomials[i],
+                make_ifelse(i + 1))
         end
     end
     make_ifelse(1)
 end
-
-### WHY ARE ALL OF THE COEFFICIENTS AVOVE NaNs?
-
 
 symbolic_cubic_spline(loop::Loop) = symbolic_cubic_spline(loop.poi)
 
@@ -107,10 +125,10 @@ function symbolic_cubic_spline(poi::PointsOfInterest)
     looped = poi[1].p == typemin(KnotParameter) &&
         poi[end].p == typemax(KnotParameter) &&
         spatial_coordinates(poi[1]) == spatial_coordinates(poi[end])
-    [ symbolic_cubic_spline(map(poi -> [poi.p, poi.x], poi), looped),   # x
-      symbolic_cubic_spline(map(poi -> [poi.p, poi.y], poi), looped),   # y
-      symbolic_cubic_spline(map(poi -> [poi.p, poi.z], poi), looped)    # z
-    ]
+    Symbolics.Arr([ symbolic_cubic_spline(map(poi -> [poi.p, poi.x], poi), looped),   # x
+                    symbolic_cubic_spline(map(poi -> [poi.p, poi.y], poi), looped),   # y
+                    symbolic_cubic_spline(map(poi -> [poi.p, poi.z], poi), looped)    # z
+                    ])
 end
 
 
@@ -137,25 +155,25 @@ three_points =
     let
         op = NoOp()
         [
-            PointOfInterest(KnotParameter(0.0), 0,  0,  0, :point1, op),
-            PointOfInterest(KnotParameter(0.3), 2,  1,  0, :point2, op),
-            PointOfInterest(KnotParameter(0.7), 2,  -1,  0, :point3, op),
+            PointOfInterest(KnotParameter(0//1), 0,  0,  0, :point1, op),
+            PointOfInterest(KnotParameter(1//3), 2,  1,  0, :point2, op),
+            PointOfInterest(KnotParameter(2//3), 2,  -1,  0, :point3, op),
+            PointOfInterest(typemax(KnotParameter), 0, 0, 0, :closed, op)
         ]
     end
-
 
 one_crossing =
     let
         op = NoOp()
         Loop([
-            PointOfInterest(KnotParameter(0.0),     -2,  0,  0,   :left, op),
-            PointOfInterest(KnotParameter(0.1),     -1,  1,  0,   :point2, op),
-            PointOfInterest(KnotParameter(0.2),      0,  0,  0.5, :cross1a, op),
-            PointOfInterest(KnotParameter(0.3),      1, -1,  0,   :point4, op),
-            PointOfInterest(KnotParameter(0.4),      2,  0,  0,   :right, op),
-            PointOfInterest(KnotParameter(0.5),      1,  1,  0,   :point6, op),
-            PointOfInterest(KnotParameter(0.6),      0,  0, -0.5, :cross1b, op),
-            PointOfInterest(KnotParameter(0.7),     -1, -1,  0,   :point8, op),
+            PointOfInterest(KnotParameter(0//8),     -2,  0,  0,   :left, op),
+            PointOfInterest(KnotParameter(1//8),     -1,  1,  0,   :point2, op),
+            PointOfInterest(KnotParameter(2//8),      0,  0,  1/2, :cross1a, op),
+            PointOfInterest(KnotParameter(3//8),      1, -1,  0,   :point4, op),
+            PointOfInterest(KnotParameter(4//8),      2,  0,  0,   :right, op),
+            PointOfInterest(KnotParameter(5//8),      1,  1,  0,   :point6, op),
+            PointOfInterest(KnotParameter(6//8),      0,  0, -1/2, :cross1b, op),
+            PointOfInterest(KnotParameter(7//8),     -1, -1,  0,   :point8, op),
             PointOfInterest(typemax(KnotParameter), -2,  0,  0,   :end, op)
         ], op)
     end
@@ -166,26 +184,31 @@ one_crossing_function = symbolic_cubic_spline(one_crossing)
 # @variables one_crossing_projection(x::KnotParameter)
 one_crossing_projection = project([0, 0, 1], one_crossing_function)
 
+
 # Can we make callable versions of one_crossing_function and
 # one_crossing_projection that can still be manipulated symbolically?
-
-
-    
-
 
 
 # We want to find two different values of the parameter that map to
 # the same point.  To do this we substitute new variables p1 and p2
 # for p in one_crossing_projection, set the two resulting projections
 # equal, and solve for p1 and p2.
-#=
 function find_crossings(projection)
-    sort(collect(union(Set(), map(Symbolics.get_variables, projection))))
+    #=
+    println("find_crossings variables: ",
+            sort(collect(union(Set(), map(Symbolics.get_variables, projection)));
+                 by v -> v.name))
+    =#
     @variables p1, p2
-    substitute(projection, Dict(getvar(:p) => p1)) ~
-        substitute(projection, Dict(getvar(:p) => p2))
+    kp = getvar(projection, :kp)
+    eq = substitute(projection, Dict(kp => p1); fold = SYMBOLIC_CUBIC_SPLINES_FOLD) ~
+        substitute(projection, Dict(kp => p2); fold = SYMBOLIC_CUBIC_SPLINES_FOLD)
+    # But these equations are not linear, they're cubic.
+
+    # Also, for multiple crossings we hpe for multiple solutions.  Do
+    # we need to try to solve an equation for the cross-product of the
+    # loop segments?
 end
-=#
 
 
 ##### Debugging
@@ -206,212 +229,17 @@ function few_unknowns(e = equations)
            collect(enumerate(e)))
 end
 
+#=
 getvar(expr, v::Symbol) = first(filter(x -> x.name == v,
                                        collect(union(Set(),
                                                      map(Symbolics.get_variables, EQUATIONS)...))))
-
-
-
-#=   NEW SOLUTION ATTEMPT:
-
-few_unknowns()
-1-element Vector{Tuple{Int64, Any}}:
- (1, 0 ~ c1*KnotParameter(Float16(0.0)) + d1)
-
-###   d1 ~ 0.0
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :d1) => 0.0))
-end
-
-# few_unknowns()  now has no meaningful results
-
-
-
-
-
 =#
 
-#=     OLD SOLUTION ATTEMPT:
-
-#  (31, 0 ~ 6a1*KnotParameter(Float16(0.0)) + 2b1)
-equations = map(equations) do e
-    substitute(e, Dict(getvar(:b1) => (getvar(:a1) * -6 * KnotParameter(Float16(0.0))) / 2))
-end
-
-#   (32, 0 ~ 6a8*KnotParameter(Float16(0.7)) + 2b8)
-equations = map(equations) do e
-    substitute(e, Dict(getvar(:b8) => -2.102))
-end
-
-filter(e -> length(Symbolics.get_variables(e[2])) <= 2, collect(enumerate(equations)))
-
-
-=#
+getvar(expr, v::Symbol) = first(filter(x -> x.name == v,
+                                       Symbolics.get_variables(expr)))
 
 #=
-
-X:
-
-symbolic_cubic_spline(map(poi -> [poi.p, poi.x], one_crossing.poi), false)
-
-equations = EQUATIONS
-
-collect(enumerate(equations))
-
-VARIABLES = sort(collect(union(Set(), map(Symbolics.get_variables, EQUATIONS)...));
-                 by = v -> v.name)
-
-### 27
-filter(e -> length(Symbolics.get_variables(e[2])) <= 2, collect(enumerate(equations)))
-
-solve_for(equations[27], [getvar(equations[27], :b1)])
-1-element Vector{Float16}:
- -0.0
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :b1) => 0.0))
-end
-
-### 1
-solve_for(equations[1], [getvar(equations[1], :d1)])
-1-element Vector{Float16}:
- -2.0
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :d1) => -2.0))
-end
-
-### 28
-solve_for(equations[28], [getvar(equations[28], :b7)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- -2.102a7
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :b7) =>  -2.102 * getvar(e, :a7)))
-end
-
-solve_for(equations[28], [getvar(equations[28], :a7)])
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :a7) => 0.0))
-end
-
-###
- (13, 0 ~ c7*KnotParameter(Float16(0.6)) + d7)
- (14, -1 ~ c7*KnotParameter(Float16(0.7)) + d7)
-
-solve_for(equations[13], [getvar(equations[13], :d7)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- -0.6c7
-
-julia> solve_for(equations[14], [getvar(equations[14], :d7)])
-solve_for(equations[14], [getvar(equations[14], :d7)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- -1 - 0.7c7
-
--0.6c7 ~  -1 - 0.7c7
-0.6c7 ~  1 + 0.7c7
--1 = 0.1c7
-c7 ~ -10
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :c7) => -10.0))
-end
-
-###
-filter(e -> length(Symbolics.get_variables(e[2])) <= 2, collect(enumerate(equations)))
- (1, -2 ~ -2.0 + c1*KnotParameter(Float16(0.0)))
- (2, -1 ~ -2.0 + 0.000999a1 + c1*KnotParameter(Float16(0.1)))
- (13, 0 ~ -6.0009765625 + d7)
- (14, -1 ~ -7.001953125 + d7)
- (26, 6a6*KnotParameter(Float16(0.6)) + 2b6 ~ 0.0)
- (27, 0 ~ 6a1*KnotParameter(Float16(0.0)))
- (28, 0 ~ 0.0)
-
-solve_for(equations[13], [getvar(equations[13], :d7)])
-1-element Vector{Float64}:
- 6.0009765625
-
-solve_for(equations[14], [getvar(equations[14], :d7)])
-1-element Vector{Float64}:
- 6.001953125
-
-### 2
-solve_for(equations[2], [getvar(equations[2], :a1)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- 1.0005e3(1.0 - 0.1c1)
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :a1) => 1.0005e3 * (1.0 - 0.1 * getvar(e, :c1))))
-end
-
-### 2
-solve_for(equations[2], [getvar(equations[2], :c1)])
-1-element Vector{Float64}:
- -2.5490196078420464
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :c1) => -2.5490196078420464))
-end
-
-### 16
-solve_for(equations[16], [getvar(equations[16], :b2)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- 0.5(753.1337316175816 - 0.5996a2)
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :b2) => 0.5(753.1337316175816 - 0.5996 * getvar(e, :a2))))
-end
-
-### 26
-solve_for(equations[26], [getvar(equations[26], :b6)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- -1.801a6
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :b6) =>  -1.801 * getvar(e, :a6)))
-end
-
-### 25
-solve_for(equations[25], [getvar(equations[25], :a6)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- 0.9246642448409784(10.0 + c6)
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :a6) => 0.9246642448409784 * (10.0 + getvar(e, :c6))))
-end
-
-### 11, 12, d6
-solve_for(equations[11], [getvar(equations[11], :d6)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- 1 + 0.3007470456345282(10.0 + c6) - 0.5c6
-
-solve_for(equations[12], [getvar(equations[12], :d6)])
-1-element Vector{SymbolicUtils.BasicSymbolic{Real}}:
- 0.39990712722892385(10.0 + c6) - 0.6c6
-
-equations = map(equations) do e
-    substitute(e, Dict(getvar(e, :d6) =>  0.39990712722892385(10.0 + getvar(e, :c6)) - 0.6 * getvar(e, :c6)))
-end
-
-
-WHAT ABOUT THE PROBLEM WITH d7
-
- (13, 0 ~ 0.2161a7 + 0.36b7 + c7*KnotParameter(Float16(0.6)) + d7)
- (14, -1 ~ 0.3433a7 + 0.4902b7 + c7*KnotParameter(Float16(0.7)) + d7)
-
-symbolic_linear_solve(EQUATIONS[13], [getvar(EQUATIONS[13], :d7)])
-d7 ~  -0.2161a7 - 0.36b7 - 0.6c7
-
-symbolic_linear_solve(EQUATIONS[14], [getvar(EQUATIONS[14], :d7)])
-d7 ~  -1 - 0.3433a7 - 0.4902b7 - 0.7c7
-
-substitute(e, Dict(getvar(e, :a7) => 0.0))
-ubstitute(e, Dict(getvar(e, :b7) =>  -2.102 * getvar(e, :a7)))
-
-
-
-
-sort(collect(union(Set(), map(Symbolics.get_variables, equations)...));
-                 by = v -> v.name)
-
+getvar(exprs::Vector{Num}, v::Symbol) = first(filter(x -> x.name == v,
+                                            union(Set(), map(Symbolics.get_variables, (exprs))...)))
 =#
+
