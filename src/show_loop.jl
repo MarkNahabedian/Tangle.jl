c66dce378bb2ca39d8d083d146a912bb8824173d
MarkNahabedian <naha@mit.edu>
2026-03-17 09:28:35 -0400

    Attempt to close loop renderings.  Improved but not quite closed yet.  Might the remaining gap be a Blender path issue?

--- src/show_loop.jl
+++ src/show_loop.jl
@@ -61,13 +61,14 @@ Returns a list of spatial coordinates of the points of interest of
 by knot parameter.
 """
 function loop_points_for_graph(loop::Loop, steps_between_poi)
+    println("loop_points_for_graph")
     params = Rational[]    # Real[]
     for i in 1 : (length(loop.poi) - 1)
-        poi1 = loop.poi[i]
-        poi2 = loop.poi[i+1]
-        push!(params, poi1.p)
-        incr = ((poi2.p^1) - poi1.p.p) // (steps_between_poi + 1)
-        for p in (poi1.p.p + incr) : incr : ((poi2.p^1) - incr)
+        poi1p = loop.poi[i].p.p
+        poi2p = loop.poi[i+1].p^1    # Deal with typemax(::KnotParameter)
+        incr = (poi2p - poi1p) // (steps_between_poi + 1)
+        println("\t$poi1p, $poi2p, incr = $incr")
+        for p in poi1p : incr : poi2p
             push!(params, p)
         end
     end
